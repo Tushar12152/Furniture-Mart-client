3fd4917f37f156d6384a3bbbeffeeb63a233f94a
Tushar12152 <tusharimran470@gmail.com>
2024-03-29 16:53:53 +0600

    footer added

--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Nav from "../Components/Nav";
+import Footer from "../Components/Footer";
 
 const Layout = () => {
     return (
         <div className="w-[95%] mx-auto">
             <Nav/>
              <Outlet/>
+             <Footer/>
         </div>
     );
 };
